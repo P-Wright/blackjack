73669486504b33bf49e2aa45c52b41d9a6508fd4
Phil Wright <phil@wrightspace.com>
2018-06-24 12:28:12 -0500

    first test passing

---
+++ include/BlackJack.hpp
@@ -0,0 +1,16 @@
+class Card {
+
+public:
+    enum Rank {
+        RANK_QUEEN
+    };
+
+    enum Suit {
+        SUIT_HEART
+    };
+
+    Card(Card::Rank rank, Card::Suit suit) {
+
+    }
+
+};
---
+++ test/BlackJackTests.cpp
@@ -0,0 +1,10 @@
+#include "BlackJack.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+    TEST(BlackJack, CardCreateTest) {
+
+    const Card cardQueenHearts(Card::RANK_QUEEN, Card::SUIT_HEART);
+
+    }
+}
